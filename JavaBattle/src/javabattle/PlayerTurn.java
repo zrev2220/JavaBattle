d4363682de4b718e7406e9b036233b15270a98ff
zrev2220 <zrev2220@gmail.com>
2017-01-02 23:43:53 -0500

    Started PlayerTurn class

---
+++ JavaBattle/src/javabattle/PlayerTurn.java
@@ -0,0 +1,25 @@
+package javabattle;
+
+/**
+ *
+ * @author Zac Hayes
+ */
+public class PlayerTurn
+{
+	public PlayerData user;
+	public PlayerData target;
+	public Move move;
+	public boolean hit;
+	public boolean smaaaash;
+	public int damage;
+	public boolean executed;
+	/* TODO Un-comment when these classes exist
+	public StatusCondition condition;
+	public StatChangeThingy statThing;
+	public DefenseThingy defenseThing;
+	*/
+	
+	public PlayerTurn()
+	{
+	}
+}
